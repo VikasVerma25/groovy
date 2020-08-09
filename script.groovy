eaf2b2351f2dc1e3ed312cf8929be973022309f9
Vikas Verma <64186894+VikasVerma25@users.noreply.github.com>
2020-08-09 20:31:33 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,96 +1,44 @@
-job("job1") {
-	description("launch k8s container, attach pvc, expose it")
-	
-	scm {
-		github('VikasVerma/devopstask6','master')
-	}
-	
-	triggers {
-		scm("* * * * *")
-		upstream("job2", 'SUCCESS')
+job('Job1') {
+    description('launching pod on K8s and deploying code to webserver')
+    steps {
+        shell(""" if kubectl get pods | grep webpage
+then
+echo "alraedy running"
+else
+kubectl create -f /workspace/webpage.yaml
+sleep 10
+fi
+podname=$(kubectl get pod -l app=webpage -o jsonpath="{.items[0].metadata.name}")
+kubectl cp /workspace/*.html $podname:/usr/local/apache2/htdocs/ """)
     }
-	
-	steps {
-	
-		shell("""
-if sudo ls . | grep -E 'html|php'
-then
-sudo cp -r /root/devt6/DockerfileWeb/Dockerfile .
-else
-sudo cp -r /root/devt6/DockerfilePy/Dockerfile .
-fi
-echo "done copy dockerfile"
-""")
-	
-		shell("""
-sudo docker build -t ishandsce/task6os .
-echo "build done"
-sudo docker push ishandsce/task6os
-echo "push done"
-""")
-	
-		shell("""
-if sudo ls . | grep -E 'html|php'
-then
-sudo kubectl apply -k /root/devt6/yaml-web/.
-podname=\$(sudo kubectl get pod | grep testpod | cut -d" " -f1)
-sudo kubectl cp index.php default/\$podname:/var/www/html/
-else
-sudo kubectl apply -k /root/devt6/yaml-web/.
-podname=\$(sudo kubectl get pod | grep testpod | cut -d" " -f1)
-sudo kubectl cp index.py default/\$podname:/app/.
-fi
-""")
-
-	}
-
 }
-
-
-job("jobt6-job3") {
-	description("testing")
-	
-	triggers {
-		upstream("jobt6-job1", 'SUCCESS')
-	}
-	
-	steps {
-	
-		shell("""
-status=\$(sudo curl -o /dev/null -w "%{http_code}" -s 192.168.99.101:30000/)
-if [[ \$status == 200 ]]
-then
-echo "Running fine!"
-else
-echo "Problem!"
-exit 1
-fi
-""")
-
-	}
-	
-	publishers {
-        extendedEmail {
-            triggers {
-                failure {
-                    sendTo {
-                        recipientList("ishandsce@gmail.com")
-                    }
-                }
-            }
+job('Job2') {
+    description('Testing the code and send notification mail')
+    triggers {
+        scm('@daily')
+        upstream {
+            upstreamProjects('job1')
+            threshold('SUCCESS')
         }
     }
-
+    steps {
+        shell(""" status=$(curl -o /dev/null -s -w "{%http_code}" http://192.168.99.101:32033)
+if [[ $status==200 ]]
+then
+python3 /workspace/success.py
+else
+python3 /workspace/fail.py
+fi """)
+    }
 }
 
-deliveryPipelineView("Task6view") {
-	allowPipelineStart(true)
-	allowRebuild(true)
-	columns(1)
-	pipelineInstances(3)
-	pipelines {
-		component("devt6pipe", "devt6-job2")
-	}
-	updateInterval(2)
-	
+buildPipelineView('Task 6') {
+    filterBuildQueue()
+    filterExecutors()
+    title('deploy webpage over k8s cluster')
+    displayedBuilds(5)
+    selectedJob('Job1')
+    alwaysAllowManualTrigger()
+    showPipelineParameters()
+    refreshFrequency(60)
 }
