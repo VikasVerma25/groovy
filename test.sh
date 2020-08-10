93dc7613aa609ee2b38e74f52efabbfa5bfc032a
Vikas Verma <64186894+VikasVerma25@users.noreply.github.com>
2020-08-10 14:01:18 +0530

    Delete test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-status=$(curl -o /dev/null -s -w "%{http_code}" http://192.168.99.101:30303)
-if [[ $status==200 ]]
-then
-    python3 /workspace/success.py
-else
-    python3 /workspace/fail.py
-fi
